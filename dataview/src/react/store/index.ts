1b7e9ae85729043682101bd1b7f7c5a9c272a3ca
RealRong <yywz.hero@gmail.com>
2026-04-09 00:44:50 +0800

    refactor: modularize shared library by migrating core utilities into dedicated dom, react, scheduler, and store packages.

--- dataview/src/react/store/index.ts
+++
@@ -1,7 +0,0 @@
-export { useExternalValue } from './useExternalValue'
-export {
-  createLazySelectorSnapshot,
-  useLazySelectorValue,
-  type LazySelectorSource
-} from './useLazySelectorValue'
-export { useKeyedStoreValue, useStoreValue } from './useStoreValue'

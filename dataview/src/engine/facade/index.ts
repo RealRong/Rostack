13b0174d6f450b0ea54ced1933d9a4e858130a3c
RealRong <yywz.hero@gmail.com>
2026-04-13 14:26:57 +0800

    refactor: overhaul engine architecture by replacing facade/store layers with a new service-based state and derivation system.

--- dataview/src/engine/facade/index.ts
+++
@@ -1,4 +0,0 @@
-export { createFieldsEngineApi } from './fields'
-export { createRecordsEngineApi } from './records'
-export { createActiveEngineApi } from './view'
-export { createViewsEngineApi } from './views'

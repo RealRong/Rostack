25bb9898b4d185603c1889d236d4cfae6f0584b5
RealRong <yywz.hero@gmail.com>
2026-04-14 07:17:26 +0800

    chore: update vitest tsconfig-paths configuration and remove dataview app build script

--- shared/dom/vitest.config.ts
+++ shared/dom/vitest.config.ts
@@ -2,7 +2,10 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  plugins: [tsconfigPaths({
+    projects: ['../../tsconfig.base.json'],
+    loose: true
+  })],
   test: {
     environment: 'node',
     include: ['test/**/*.test.ts'],
